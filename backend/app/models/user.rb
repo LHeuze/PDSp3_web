b596923c4b6cb10dae12e9461b4d0f74cc18b687
LHeuze <lheuze@miuandes.cl>
2024-11-06 12:08:03 -0300

    backend?

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  
+  before_create :set_default_role
+  enum role: { locker_admin: 'locker_admin', superuser: 'superuser' }
+
+  private
+
+  def set_default_role
+    self.role ||= :locker_admin
+  end
 end
