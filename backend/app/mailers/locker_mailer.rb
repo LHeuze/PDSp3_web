429b8e11f916999fc54d54f3e25eb55c0d3b91bf
LHeuze <lheuze@miuandes.cl>
2024-11-12 19:20:40 -0300

    images attached to mail + instructions

--- backend/app/mailers/locker_mailer.rb
+++ backend/app/mailers/locker_mailer.rb
@@ -3,6 +3,9 @@ class LockerMailer < ApplicationMailer
     @locker = locker
     @owner_email = @locker.owner_email
     @new_password = @locker.password
+    @new_password.each do |gesture|
+      attachments.inline["#{gesture.downcase}.jpg"] = File.read("app/assets/images/gestures/#{gesture.downcase}.jpg")
+    end
     mail(to: @owner_email, subject: 'Your Locker Password Has Been Updated')
   end
 end
