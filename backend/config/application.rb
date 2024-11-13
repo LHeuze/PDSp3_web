094b8cbe4d4c41aad93a26a44cb196ff21065430
LHeuze <lheuze@miuandes.cl>
2024-11-12 23:51:32 -0300

    prepare deploy

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -39,7 +39,7 @@ module PdSp3
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    #config.api_only = true
     config.public_file_server.enabled = true
   end
 end
